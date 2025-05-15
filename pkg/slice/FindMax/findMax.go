7a80344eac40e922ea3d2f18a07bece13b75e175
Xetcer <ferdinand_tema@mail.ru>
2025-05-15 12:44:51 +0500

    feat: add FindMax function with recursion

---
+++ pkg/slice/FindMax/findMax.go
@@ -0,0 +1,22 @@
+package findmax
+
+// FindMax рекурсивный поиск максимального элемента в массиве
+func FindMax(s []int) int {
+	// базовый случай
+	switch len(s) {
+	case 0:
+		return 0 // базовый случай
+	case 1:
+		return s[0] // базовый случай
+	default: // рекурсивный случай
+		{
+			findedMax := FindMax(s[1:])
+			if s[0] >= findedMax {
+				return s[0]
+			} else {
+				return findedMax
+			}
+		}
+	}
+
+}
